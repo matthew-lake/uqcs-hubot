170ab09003451d2849be2cb6bb7211a26f634307
matthew-lake <mgtlake@outlook.com>
2016-01-12 22:56:32 +1000

    Add reddit-linker

---
+++ scripts/linker.js
@@ -0,0 +1,31 @@
+// Description
+//   Creates helpful links - currently listens for 'r/whatever' and sends the reddit link
+//
+var https = require("https");
+
+module.exports = function (robot) {
+
+	// Reddit links
+	robot.hear(/\/(.+)/i, function (res) {
+		var url = "https://www.reddit.com/r/" + res.match[1];
+		exists(url)
+			.then(function() {
+				res.send(`Link for the lazy: ${url}`);
+			});
+	});
+
+	// Checks if the link target exists
+	var exists = function (url) {
+		return new Promise(function(resolve, reject) {
+			https.get(url, function (resp, err) {
+				if (err) {
+					reject();
+				} else if (resp.statusCode === 200) {
+					resolve();
+				}
+				reject();
+			});
+		});
+	};
+
+};
